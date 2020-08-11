2f0777c4c90b92b496ef38014ca94d76264462ab
AungKMin <49665483+AungKMin@users.noreply.github.com>
2020-08-11 16:06:50 -0400

    create headers

--- app/routes/homeStack.js
+++ app/routes/homeStack.js
@@ -8,11 +8,36 @@ import Resources from "../screens/Resources/index.js";
 import SimplyNeurocon from "../screens/SimplyNeurocon/index.js";
 import { StyleSheet, Dimensions } from "react-native";
 
+import colors from "../config/colors";
+
 const HomeStack = createStackNavigator();
 
 const styles = StyleSheet.create({
-  header: {
+  header1: {
     height: Dimensions.get("window").height * 0.1,
+    backgroundColor: colors.white,
+  },
+  header2: {
+    height: Dimensions.get("window").height * 0.1,
+    backgroundColor: colors.darkBlue,
+  },
+  headerTitle1: {
+    color: colors.darkBlue,
+    fontSize: 20,
+    //fontFamily: "Raleway",
+    fontWeight: "bold",
+  },
+  headerTitle2: {
+    color: colors.white,
+    fontSize: 20,
+    //fontFamily: "Raleway",
+    fontWeight: "bold",
+  },
+  headerBackTitle1: {
+    color: "black",
+  },
+  headerBackTitle2: {
+    color: colors.white,
   },
 });
 
@@ -23,12 +48,52 @@ export default function MyStack() {
         <HomeStack.Screen
           name="Home"
           component={Home}
-          options={{ headerStyle: styles.header }}
+          options={{
+            headerStyle: styles.header1,
+            headerTitleStyle: styles.headerTitle1,
+            headerBackTitleStyle: styles.headerBackTitle1,
+          }}
         />
-        <HomeStack.Screen name="About" component={About} />
-        <HomeStack.Screen name="Advice" component={Advice} />
-        <HomeStack.Screen name="Resources" component={Resources} />
-        <HomeStack.Screen name="SimplyNeurocon" component={SimplyNeurocon} />
+        <HomeStack.Screen
+          name="About"
+          component={About}
+          options={{
+            headerStyle: styles.header1,
+            headerTitleStyle: styles.headerTitle1,
+            headerBackTitleStyle: styles.headerBackTitle1,
+            title: "about us",
+          }}
+        />
+        <HomeStack.Screen
+          name="Advice"
+          component={Advice}
+          options={{
+            headerStyle: styles.header2,
+            headerTitleStyle: styles.headerTitle2,
+            headerBackTitleStyle: styles.headerBackTitle2,
+            title: "annoymous advice",
+          }}
+        />
+        <HomeStack.Screen
+          name="Resources"
+          component={Resources}
+          options={{
+            headerStyle: styles.header1,
+            headerTitleStyle: styles.headerTitle1,
+            headerBackTitleStyle: styles.headerBackTitle1,
+            title: "explore our resources",
+          }}
+        />
+        <HomeStack.Screen
+          name="SimplyNeurocon"
+          component={SimplyNeurocon}
+          options={{
+            headerStyle: styles.header2,
+            headerTitleStyle: styles.headerTitle2,
+            headerBackTitleStyle: styles.headerBackTitle2,
+            title: "simply neurocon",
+          }}
+        />
       </HomeStack.Navigator>
     </NavigationContainer>
   );
